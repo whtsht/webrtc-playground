f5bc10a608238946ae6bd79cdc3891ece95bb59f
whtsht <whiteshirt0079@gmail.com>
2023-10-17 09:13:16 +0900

    add cors

--- ap/src/app.ts
+++ ap/src/app.ts
@@ -1,5 +1,13 @@
 import { Server } from "http";
+import { Server as SocketIO } from "socket.io";
 
 const httpServer = new Server();
+
+const io = new SocketIO(httpServer, {
+    cors: {
+        origin: "http://localhost:5173",
+        methods: ["GET", "POST"],
+    },
+});
 
 httpServer.listen(8080);
